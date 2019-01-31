df94a29faf3e3bc38b2f8a29882e7de90e0ea1c0
Martin Bagshaw <martinbagshaw1@gmail.com>
2019-01-31 18:20:36 +0000

    made it move, sort of

--- src/components/Snake.js
+++ src/components/Snake.js
@@ -6,18 +6,53 @@ export default class Snake extends React.Component {
         super(props);
 
         this.time = props.time;
+        this.snake = props.snakeArr;
+        this.update = props.update;
 
-        this.state = {
-            snakeArr: [[0,0],[1,0],[2,0]],
-            time: 0
-        };
     }
 
 
     // links interval timer to render
+    // - runs every 0.5s
     shouldComponentUpdate(nextProps) {
+
+        // console.log(this.update);
+        
         const newTime = this.time !== nextProps.time;
-        // console.log('time');
+
+    //     if (newTime) {
+    //         props.update(props.snakeArr)
+    //     }
+        
+    //     // console.log(nextProps);
+    //     // console.log(this.snake);
+
+        const cloneSnake = Array.from(this.snake);
+        const snakeHead = cloneSnake[cloneSnake.length - 1];
+
+        // update the x value
+        const newX = snakeHead[0]++;
+        const newHead = [newX, snakeHead[1]];
+
+        // console.log(cloneSnake.push(newHead));
+
+        const a = [...cloneSnake, ...[newHead]];
+        console.log(a);
+
+
+        // this.snake = [cloneSnake, ...newHead];
+
+        // const a = cloneSnake.concat(newHead);
+        // console.log(a);
+
+        
+    //     // console.log(newHead);
+
+    //     // this.setState({snakeArr: [cloneSnake, ...newHead]})
+
+    //     // update the state here with setState
+
+
         return newTime;
     }
 
@@ -38,7 +73,9 @@ export default class Snake extends React.Component {
 
     render(){
 
-        const snakeCells = this.state.snakeArr.map((item, index) => {
+        // console.log(this.time);
+
+        const snakeCells = this.snake.map((item, index) => {
             // get x and y co-ordinates from each array item
             const style =  {
                 left: `${item[0] * 5}vw`,
