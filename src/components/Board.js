ea560d0b45305eef4c60cf47b1de3213613ebed2
Martin Bagshaw <martinbagshaw1@gmail.com>
2019-01-31 13:04:26 +0000

    first accurate branch name!

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import Snake from './Snake';
 
-export default class Board extends React.Component {
+// export default class Board extends React.Component {
 
-    state = {
-    };
+//     state = {
+//     };
 
-    render() {
-        return (
-            <div className="container game">
-                <div className="board">
-                <Snake />
-                {/* <div className="snake-cell"></div> */}
-                </div>
+//     render() {
+//         return (
+//             <div className="container game">
+//             {props.time}
+//                 <div className="board">
+//                 <Snake time={props.time}/>
+//                 {/* <div className="snake-cell"></div> */}
+//                 </div>
+//             </div>
+//         )
+//     }
+
+
+// }
+
+
+// get the props from the parent component's state
+const Board = (props) => (
+    <div className="container game">
+        {/* {props.time} */}
+            <div className="board">
+            <Snake time={props.time}/>
             </div>
-        )
-    }
+        </div>
+)
 
-
-}
+export default Board;
