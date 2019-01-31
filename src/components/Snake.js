ccdec7266cc815e05db5ed638b07d76e5f48ee0a
Kristina Jaggard <jaggardk@gmail.com>
2019-01-31 14:32:07 +0000

    renders page on every interval

--- src/components/Snake.js
+++ src/components/Snake.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 export default class Snake extends React.Component {
 
-    // state = {
-    //     snakeArr: [[0,0],[1,0],[2,0]],
-    //     time: 0
-    // };
     constructor(props) {
         super(props);
 
@@ -18,8 +14,14 @@ export default class Snake extends React.Component {
     }
 
 
+    // links interval timer to render
+    shouldComponentUpdate(nextProps) {
+        const newTime = this.time !== nextProps.time;
+        // console.log('time');
+        return newTime;
+    }
 
-
+    
     // snake knows to keep travelling in one direction because:
     // - we are only ever adding to item to the front of the array (the head)
     // - after 
@@ -35,6 +37,7 @@ export default class Snake extends React.Component {
 
 
     render(){
+
         const snakeCells = this.state.snakeArr.map((item, index) => {
             // get x and y co-ordinates from each array item
             const style =  {
@@ -42,11 +45,7 @@ export default class Snake extends React.Component {
                 bottom: `${item[1] * 5}vw`,
                 item: index
             }
-
-            // console.log(style);
         return style;
-
-
         })
         
         return (
--- src/components/app.js
+++ src/components/app.js
@@ -5,10 +5,6 @@ import Board from './Board.js';
 import Gif from './Gif.js';
 import StartBtn from './StartBtn.js'
 
-
-// const App = () => <h1>Hello World</h1>;
-// render(<App />, document.getElementById('root'));
-
 export default class App extends React.Component {
 
     state = {
