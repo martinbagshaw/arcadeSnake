f8da28becc1b5adc983d16e0a8b053f0b6d25fb8
Martin Bagshaw <martinbagshaw1@gmail.com>
2019-03-20 15:58:44 +0000

    hiScore first draft

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import Apple from './Apple';
-import Snake from './Snake';
-import StartBtn from './StartBtn.js'
+import React from "react";
+
+// Components
+import Apple from "./Apple";
+import Snake from "./Snake";
+import Score from "./Score";
+import StartBtn from "./StartBtn.js";
 
 // get the props from the parent component's state
 const Board = props => {
+  const { boardWidth, snakeArr, hiScore, running } = props.data;
 
-    const { boardWidth } = props.data;
-    
-    const style = {
-        width: `${boardWidth}vw`,
-        height: `${boardWidth}vw`,
-    }
+  const style = {
+    width: `${boardWidth}vw`,
+    height: `${boardWidth}vw`
+  };
 
-    // startover needs to match up with startover passed in in app.js
-    return (
-        <div className="container game">
-            <div className="board" style={style}>
-                <div className="board-border"></div>
-                <Apple data={props.data}/>
-                <Snake data={props.data}/>
-                <StartBtn onClick={props.startover} data={props.data} />
-            </div>
-        </div>
-    )
-}
+  // startover needs to match up with startover passed in in app.js
+  return (
+    <div className="container game">
+      <div className="board" style={style}>
+        <div className="board-border" />
+        <Apple data={props.data} />
+        <Snake data={props.data} />
+        <Score
+          snakeLength={snakeArr.length}
+          hiScore={hiScore}
+          running={running}
+        />
+        <StartBtn onClick={props.startover} data={props.data} />
+      </div>
+    </div>
+  );
+};
 
 export default Board;
