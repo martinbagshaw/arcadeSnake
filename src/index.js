545b2399ce23d0ffe45c74a94c740ff7968947fe
Martin Bagshaw <martinbagshaw1@gmail.com>
2019-03-21 11:48:24 +0000

    change component paths to fix netlify build

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 // may want this to be reactDOM?
 import { render } from "react-dom";
 
-import App from "./Components/App.js";
+import App from "./Components/App";
 
 render(<App />, document.getElementById("root"));
